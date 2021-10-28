5fa78b303d33dccf7f9d1e837ca76c42507fe8e0
Ronnel Pariñas <88175872+RonnelTuazonParinas@users.noreply.github.com>
2021-10-28 22:27:41 +0800

    Midterm

---
+++ 3D_Midterm_Maze_Game/Assets/Script/MeshGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    public float pyramid = 5f;
+ 
+    void Update()
+    {
+        MeshFilter meshfilter = new MeshFilter();
+
+
+    }
+}
