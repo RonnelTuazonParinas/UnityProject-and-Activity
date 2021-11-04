693cd03ef9f8881f7cbd3391c2a74f4709b02a25
Ronnel Pariñas <88175872+RonnelTuazonParinas@users.noreply.github.com>
2021-11-04 16:42:00 +0800

    3D_Game_Maze

---
+++ GameMaze/Assets/Script/EnemyReaction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyReaction : MonoBehaviour
+{
+   
+ 
+    void Update()
+    {
+
+
+    }
+    private void OnTriggerEnter3D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+   
+}
