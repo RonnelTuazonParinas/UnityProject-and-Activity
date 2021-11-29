d7d4baa2c0c3eb209dc08c6298e45394e4ffb9cc
Ronnel Pariñas <88175872+RonnelTuazonParinas@users.noreply.github.com>
2021-11-29 18:35:46 +0800

    Rotating Cube Activity

---
+++ Mobile3D/Assets/Script/RotatingCube.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCube : MonoBehaviour
+{
+    float xspeed = 0.0f;
+    float yspeed = 0.0f;
+    float zspeed = 0.0f;
+    public bool Rotate = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Rotate)
+        {
+            yspeed = 50;
+
+        }
+        else
+        {
+            yspeed = 0;
+        }
+        transform.Rotate
+            (
+            xspeed + Time.deltaTime,
+            yspeed + Time.deltaTime,
+            zspeed + Time.deltaTime
+        
+            );
+        if (Input.GetMouseButton(0))
+        {
+            Rotate = true;
+        }
+       
+    }
+
+    public void ChangeBool()
+    {
+        Rotate = !Rotate;
+
+    }
+}
